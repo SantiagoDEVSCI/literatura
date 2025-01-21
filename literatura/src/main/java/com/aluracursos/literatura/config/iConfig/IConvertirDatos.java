c35f277a5e5052237f47e1cbecee1e1a7c95b158
SantiagoDEVSCI <161984963+SantiagoDEVSCI@users.noreply.github.com>
2025-01-20 20:21:41 -0500

    Delete literatura directory

--- literatura/src/main/java/com/aluracursos/literatura/config/iConfig/IConvertirDatos.java
+++
@@ -1,7 +0,0 @@
-package com.aluracursos.literatura.config.iConfig;
-
-public interface IConvertirDatos {
-
-    <T> T convertirDatosJsonAJava(String json , Class<T> clase); //metodo generico que coje el json y convierte en datos Java ya que no se sabe que retornara
-
-}
